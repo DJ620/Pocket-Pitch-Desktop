7da100c457df1b7dfab6e61f70e860d9e9f9d7ec
DJ Hersh <DanielJHersh@gmail.com>
2023-10-31 14:48:36 -0700

    fixing things

--- Pocket-Pitch/src/App.jsx
+++ Pocket-Pitch/src/App.jsx
@@ -5,7 +5,6 @@ import MusicNotes from "./Components/Note";
 const App = () => {
   return (
     <div>
-      <h1>Pocket Pitch</h1>
       {allNotes.map((note) => {
         return (
           <div key={note.id}>
