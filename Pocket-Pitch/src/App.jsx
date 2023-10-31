43e282633c9071e844e95fd1ed98138bb5a81118
DJ Hersh <DanielJHersh@gmail.com>
2023-10-31 12:31:49 -0700

    added header

--- Pocket-Pitch/src/App.jsx
+++ Pocket-Pitch/src/App.jsx
@@ -5,6 +5,7 @@ import MusicNotes from "./Components/Note";
 const App = () => {
   return (
     <div>
+      <h1>Pocket Pitch</h1>
       {allNotes.map((note) => {
         return (
           <div key={note.id}>
