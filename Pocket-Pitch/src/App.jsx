6a98cbc9ca91eb6d090836de78dcaae629966023
DJ Hersh <DanielJHersh@gmail.com>
2023-11-02 18:48:05 -0700

    add styling and loop functionality

--- Pocket-Pitch/src/App.jsx
+++ Pocket-Pitch/src/App.jsx
@@ -1,18 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import allNotes from "./assets/AllNotes";
 import MusicNotes from "./Components/Note";
 import "./App.css";
 import "@fontsource/poppins";
 
 const App = () => {
+  const [loop, setLoop] = useState(false);
+  const [currentNote, setCurrentNote] = useState('');
+  const [playNote, setPlayNote] = useState(false);
+
+  useEffect(() => {
+    if (currentNote && playNote) {
+      document.getElementById(currentNote).play();
+    } else if (currentNote && !playNote) {
+      document.getElementById(currentNote).pause();
+    }
+  }, [loop, currentNote, playNote]);
+
+  const titleStyles = {
+    fontWeight: '800',
+    marginBottom: '-10px'
+  }
+
+  const loopStyles = {
+    background: loop ? 'none' : 'white',
+    backgroundImage: loop ? 'linear-gradient(pink, red)' : 'none',
+    color: loop ? 'white' : 'black',
+    cursor: 'pointer',
+    marginBottom: '40px',
+    borderRadius: '15px',
+    padding: '0px 15px'
+  }
+
+  const handlePlay = (note) => {
+    const lastNote = currentNote;
+    if (lastNote) {
+      document.getElementById(lastNote).pause();
+    };
+    setCurrentNote(note.id);
+    if (loop && playNote && note.id == lastNote) {
+      return setPlayNote(false);
+    };
+    setPlayNote(true);
+  };
+
+  const handlePause = () => {
+    if (loop) return;
+    setPlayNote(false);
+  };
+
+  const handleLoopClick = () => {
+    if (loop) {
+      setPlayNote(false);
+    };
+    setLoop(!loop);
+  };
+
   return (
     <div>
-      <h1 style={{ fontWeight: "800" }}>Pocket Pitch</h1>
+      <h1 style={titleStyles} className="ppTitle">Pocket Pitch</h1>
+      <h3 style={{ marginTop: '0px'}}>Pitch Pipe</h3>
+      <button style={loopStyles} onClick={() => handleLoopClick()}>Loop</button>
       <div style={{ display: "flex" }}>
         {allNotes.map((note) => {
           return (
-            <div key={note.id}>
-              <MusicNotes note={note} />
+            <div key={note.id} onMouseDown={() => handlePlay(note)} onMouseUp={() => handlePause()}>
+              <MusicNotes  note={note} loop={loop} currentNote={currentNote} playNote={playNote}/>
             </div>
           );
         })}
--- Pocket-Pitch/src/Components/Note.jsx
+++ Pocket-Pitch/src/Components/Note.jsx
@@ -1,35 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import "@fontsource/poppins/700.css";
 
-const Note = ({note}) => {
-  const [hover, setHover] = useState(false);
-    const playNote = (note) => {
-        document.getElementById(note).play();
-        setHover(true);
-      };
-    
-      const stopNote = (note) => {
-          document.getElementById(note).pause();
-          setHover(false);
-      };
+const Note = ({ note, loop, currentNote, playNote }) => {
+  const [playing, setPlaying] = useState(false);
+
+  useEffect(() => {
+    if (currentNote == note.id && playNote) {
+      setPlaying(true);
+    } else {
+      setPlaying(false);
+    };
+  }, [currentNote, loop, playNote]);
 
       const noteBtn = {
         height: '50px',
         width: '50px',
         borderRadius: '50px',
-        color: 'red',
+        color: playing ? 'white' : 'red',
         fontWeight: '700',
-        background: hover ? 'lightgray' : 'white',
+        background: playing ? 'none' : 'white',
+        backgroundImage: playing ? 'linear-gradient(pink, red)' : 'none',
         cursor: 'pointer',
         marginRight: '10px'
       }
 
   return (
     <div>
-        <button style={noteBtn} onMouseDown={() => playNote(note.id)} onMouseUp={() => stopNote(note.id)} onMouseLeave={() => stopNote(note.id)}>
+        <button style={noteBtn}>
         {note.note}
       </button>
-      <audio src={note.src} loop id={note.id}></audio>
+      <audio src={note.src} loop id={note.id} className='audio'></audio>
     </div>
   )
 }
